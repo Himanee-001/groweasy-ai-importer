8f9b22d0a561318026c35d74fcacf75d6241e4bf
himanee-001 <himaneemahajan@gmail.com>
2026-07-12 12:25:34 +0530

    Fix uploads directory for Render

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
 const { processCSV } = require("../controllers/uploadController");
 
 const router = express.Router();
 
+// Create uploads folder automatically if it doesn't exist
+const uploadPath = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, uploadPath);
   },
 
   filename: function (req, file, cb) {
